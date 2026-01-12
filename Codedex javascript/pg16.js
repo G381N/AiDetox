7dc8029c8f0c0949cc80193a5fdad44a47f60be1
g381n <gebin.official@gmail.com>
2026-01-12 12:52:09 +0530

    Add pg16.js: Implement Air Quality Index (AQI) category determination

---
+++ Codedex javascript/pg16.js
@@ -0,0 +1,20 @@
+// Air Quality Index (AQI) Category Determination
+
+let aqi=107;
+if(aqi>=0 && aqi<=50){
+    console.log("Good");
+}else if(aqi>50 && aqi<=100){
+    console.log("Moderate");
+}
+else if(aqi>100 && aqi<=150){
+    console.log("Unhealthy for Sensitive Groups");
+}
+else if(aqi>150 && aqi<=200){
+    console.log("Unhealthy");
+}   
+else if(aqi>200 && aqi<=300){
+    console.log("Very Unhealthy");
+}
+else {
+    console.log("Hazardous");
+}
