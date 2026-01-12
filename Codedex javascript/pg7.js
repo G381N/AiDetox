a468e8ecc46f38b2f6b556e963d77d428b2d44a5
g381n <gebin.official@gmail.com>
2026-01-12 12:11:55 +0530

    Add pg7.js: Declare company information variables and log their values

---
+++ Codedex javascript/pg7.js
@@ -0,0 +1,11 @@
+// Declaring variables to store company information
+
+let companyName="Hire3x";
+let foundingYear=2020;
+let isActive=true;
+let fundingAmount;
+// Printing all the data 
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
