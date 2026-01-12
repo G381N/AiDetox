dd614ddb8ba07f86c78add53fbbbfb92772922c7
g381n <gebin.official@gmail.com>
2026-01-12 13:12:04 +0530

    Add pg18.js: Implement Duck Duck Goose game simulation

---
+++ Codedex javascript/pg18.js
@@ -0,0 +1,10 @@
+//Duck Duck Goose Game Simulation
+
+let randomNumber = Math.floor(Math.random() * 10);
+
+while (randomNumber !== 7) {
+  console.log("Duck 🦆");
+  randomNumber = Math.floor(Math.random() * 10);
+}
+
+console.log("Goose! 🦢");
