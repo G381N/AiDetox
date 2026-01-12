5d750d9d4b61dd62af7aec815272c19d14ceac73
g381n <gebin.official@gmail.com>
2026-01-12 12:13:35 +0530

    Add pg4.js: Create a recipe guide for making Maggy

---
+++ Codedex javascript/pg4.js
@@ -0,0 +1,10 @@
+// Recipe to make Maggy
+
+console.log(" How to make MAGGY ")
+console.log(" 1. Add a cup of water to a pot.")
+console.log(" 2. Add the contents of the Maggy packet.")
+/*
+  Add chilly pouder and lots of pepper pouder
+*/
+console.log(" 3. Wait for 2min for the maggy to cook.")
+console.log(" 4. Server hot")
