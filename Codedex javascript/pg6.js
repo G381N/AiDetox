8aa2dbd8e27526f1905e7b04c6e1b4d53a904585
g381n <gebin.official@gmail.com>
2026-01-12 12:12:25 +0530

    Add pg6.js: Declare variables for personal information and log their values to the console

---
+++ Codedex javascript/pg6.js
@@ -0,0 +1,11 @@
+// Variable declarations and logging to console
+const firstName="Gebin George"
+const favColor="Black"
+
+let location="Bangalore";
+let mood="curious";
+
+console.log(firstName)
+console.log(favColor)
+console.log(location)
+console.log(mood)
