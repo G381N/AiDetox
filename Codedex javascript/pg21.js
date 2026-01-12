f415b649f5747cae656f64fd7ae1130b08bd0f90
g381n <gebin.official@gmail.com>
2026-01-12 15:00:39 +0530

    Add pg21.js: Implement logic to find the first odd number and stop at 42

---
+++ Codedex javascript/pg21.js
@@ -0,0 +1,14 @@
+// Finding the First Odd Number and Stopping at 42
+for(let i=0;i<50;i++)
+{ if(i==42)
+{
+  console.log(i);
+  break;
+}
+  if(i%2==0)
+  {
+    continue;
+  }
+  
+  console.log(i);
+}
