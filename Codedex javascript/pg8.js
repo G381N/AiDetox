65d9cf9266829c3f47a2cb41d12b1330b7745189
g381n <gebin.official@gmail.com>
2026-01-12 12:11:07 +0530

    Add pg8.js: Implement temperature conversion from Fahrenheit to Celsius

---
+++ Codedex javascript/pg8.js
@@ -0,0 +1,8 @@
+// Write a program to convert temperature from Fahrenheit to Celsius
+let celsius;
+let fahrenheit;
+function temp(fahrenheit){
+ celsius=(fahrenheit-32)/1.8;
+ return celsius;
+}
+console.log(temp(0));
