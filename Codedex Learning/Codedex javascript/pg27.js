169bc44773124274c94cb59955b0182f06d2a29f
g381n <gebin.official@gmail.com>
2026-01-13 11:54:56 +0530

    Add 'Where's Waldo' character search implementation

---
+++ Codedex Learning/Codedex javascript/pg27.js
@@ -0,0 +1,18 @@
+//Where's Waldo
+
+const characters = [
+  "The Wally Watchers",
+  "Wilma",
+  "Fritz",
+  "Wizard Whitebeard",
+  "Odlaw",
+  "Waldo",
+  "Woof"
+];
+
+if (characters.includes("Waldo")) {
+  const waldoIndex = characters.indexOf("Waldo");
+  console.log(`Found Waldo at index ${waldoIndex}!`);
+} else {
+  console.log("Not found");
+}   
