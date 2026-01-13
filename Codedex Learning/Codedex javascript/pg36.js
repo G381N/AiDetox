16d4e378d2567e876a49988ef253db7cefa3ae9c
g381n <gebin.official@gmail.com>
2026-01-13 12:25:32 +0530

    Add pokemon object with properties and logging for caught status

---
+++ Codedex Learning/Codedex javascript/pg36.js
@@ -0,0 +1,12 @@
+// Create an object named 'pokemon' with properties: name, type, and level
+
+let pokemon = {
+  name: "Pikachu",
+  type: "Electric ⚡️",
+  level: 25,
+}
+pokemon.isCaught = false;
+console.log(pokemon);
+pokemon.isCaught = true;
+pokemon.name = "Sparky";
+console.log(pokemon);   
