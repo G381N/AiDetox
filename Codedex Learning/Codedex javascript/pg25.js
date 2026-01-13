0fd6bc3424f21019af9dc43ef10b6741e6fe3c81
g381n <gebin.official@gmail.com>
2026-01-13 11:40:46 +0530

    Add multiplication table for 9 implementation

---
+++ Codedex Learning/Codedex javascript/pg25.js
@@ -0,0 +1,8 @@
+// Multiplication Table for 9
+
+let multiple = 9; 
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < numbers.length; i++) {
+  let answer = multiple * numbers[i];  
+  console.log(multiple + " x " + numbers[i] + " = " + answer);
+}
