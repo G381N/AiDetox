ceb290ec252cf7a48362f49e2514079f2a4d5e76
g381n <gebin.official@gmail.com>
2026-01-13 12:01:42 +0530

    Add countdown function that logs numbers from 10 to 1 and returns "Blast Off! 🚀"

---
+++ Codedex Learning/Codedex javascript/pg30.js
@@ -0,0 +1,9 @@
+// Countdown from 10 to 1 and return "Blast Off! 🚀"
+
+function countdown() {
+  for (let i = 10; i >= 1; i--) {
+    console.log(i);
+  }
+  return "Blast Off! 🚀";
+}
+console.log(countdown());
