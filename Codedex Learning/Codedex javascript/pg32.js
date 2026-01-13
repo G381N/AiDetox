d6ff96b3db95d01b995661bf2153d1bb1559bd2c
g381n <gebin.official@gmail.com>
2026-01-13 12:11:20 +0530

    Add functions to calculate mean and median of social media views; create pg33.js file

---
+++ Codedex Learning/Codedex javascript/pg32.js
@@ -0,0 +1,31 @@
+// array of recent views on different social media platforms
+
+const recentTikTokViews = [1932, 2300, 453, 5222, 6733, 7402, 8334];
+const recentInstagramViews = [936, 2576, 453, 7013, 5489, 7402, 3921];
+const recentYouTubeViews = [2300, 453, 5222, 989, 6733, 7402, 2789];
+function mean(viewsArray) {
+    let totalViews = 0;
+    for (let i = 0; i < viewsArray.length; i++) {
+        totalViews += viewsArray[i];
+    }
+    return totalViews / viewsArray.length;
+}
+
+function median(viewsArray) {
+    let sortedStats = viewsArray.slice().sort((a, b) => a - b);
+    let middleIndex = Math.floor(sortedStats.length / 2);
+    if (sortedStats.length % 2 === 0) {
+        return (sortedStats[middleIndex - 1] + sortedStats[middleIndex]) / 2;
+    } else {
+        return sortedStats[middleIndex];
+    }
+}   
+console.log("TikTok");
+console.log("Mean: " + mean(recentTikTokViews));
+console.log("Median: " + median(recentTikTokViews));
+console.log("Instagram");
+console.log("Mean: " + mean(recentInstagramViews));
+console.log("Median: " + median(recentInstagramViews));
+console.log("YouTube");
+console.log("Mean: " + mean(recentYouTubeViews));
+console.log("Median: " + median(recentYouTubeViews));
