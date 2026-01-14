a765a0562331739aee5a461ac79a510dc3cfc4d1
g381n <gebin.official@gmail.com>
2026-01-14 18:26:57 +0530

    refactor: streamline user update functions for improved clarity and efficiency

--- TASK/Day4/practice/pg6.js
+++ TASK/Day4/practice/pg6.js
@@ -4,21 +4,11 @@ let usersArray = [
     { id: 2, name: "tony", age: 30 },
     { id: 3, name: "banner", age: 35 }
 ];
-function updateUserAge(arr, userId, newAge) {
-    let updatedArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].id === userId) {
-            let updatedUser = {
-                id: arr[i].id,
-                name: arr[i].name,
-                age: newAge
-            };
-            updatedArray.push(updatedUser);
-        } else {
-            updatedArray.push(arr[i]);
-        }
-    }
-    return updatedArray;
+
+function updateUserNameById(arr, id, newName) {
+    return arr.map(user => 
+        user.id === id ? { ...user, name: newName } : user
+    );
 }
-console.log(updateUserAge(usersArray, 2, 31));
-console.log(usersArray);
+let updatedUsers = updateUserNameById(usersArray, 2, "stark");
+console.log(updatedUsers);
--- TASK/Day4/practice/pg7.js
+++ TASK/Day4/practice/pg7.js
@@ -1,14 +1,10 @@
 // Given an array of user objects, return a new array containing only user names in uppercase.
-let users = [
-    { id: 1, name: "gebin", age: 25 },
-    { id: 2, name: "tony", age: 30 },
-    { id: 3, name: "banner", age: 35 }
-];
-function getUppercaseNames(userArray) {
-    let uppercaseNames = [];
-    for (let i = 0; i < userArray.length; i++) {
-        uppercaseNames.push(userArray[i].name.toUpperCase());
-    }
-    return uppercaseNames;
+function getUppercaseUsernames(users) {
+  return users.map(user => user.name.toUpperCase());
 }
-console.log(getUppercaseNames(users));
+
+getUppercaseUsernames([
+  { name: "Gebin" },
+  { name: "alex" }
+]);
+    
