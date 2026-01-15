bfa83bdb9b8b92d168cd2e3b77cb589252cd21f5
g381n <gebin.official@gmail.com>
2026-01-15 22:51:52 +0530

    feat: Add BandTour component to display tour stops with locations

---
+++ Codedex Learning/Codedex React/6.BandTour.js
@@ -0,0 +1,14 @@
+export default function App() {
+  return <div> {
+    <div>
+      <h1>Tour Stops</h1>
+      <ul>
+      <li>🇺🇸 New York City, US - Madison Square Garden</li>
+      <li>🇬🇧 London, UK - Wembley Stadium</li>
+      <li>🇩🇪 Munich, DE - Zenith Halle</li>
+      <li>🇯🇵 Tokyo, JP - Budokan</li>
+      <li>🇦🇺 Melbourne, AU - The Forum</li>
+</ul>
+    </div>
+  } </div>;
+}
