b58d9a470c0b15d061436e81d885f0238d8e43c8
g381n <gebin.official@gmail.com>
2026-01-20 10:53:18 +0530

    feat: update UserItem and UserList components for improved user management functionality

--- user-manager-app/src/components/UserItem.vue
+++ user-manager-app/src/components/UserItem.vue
@@ -1,21 +1,65 @@
 <template>
-  <div class="user-item">
+  <li class="user-item">
+    <span>{{ user.name }} - {{ user.email }} (Age: {{ user.age }})</span>
+    <!-- Displaying user data from 'user' prop using mustache syntax -->
     
-  </div>
+    <button v-on:click="deleteUser">Delete</button>
+    <!-- v-on:click calls deleteUser method when button is clicked -->
+  </li>
 </template>
 
 <script>
-export default {
+export default 
+{
   name: 'UserItem',
-  props: {
-    
+  
+  props: 
+  {
+    user: {
+      type: Object,   // Expects a user object with name, email, age
+      required: true,
+    },
+    index: {
+      type: Number,   // Position of this user in the array
+      required: true,
+    },
   },
-  methods: {
-    
-  }
-}
+  // Props received from parent (UserList.vue) via v-bind:user and v-bind:index
+  
+  methods: 
+  {
+    deleteUser() // Called when delete button is clicked
+    {
+      this.$emit('delete-user', this.index);
+      // Emits 'delete-user' event UP to parent (UserList.vue) with the index
+    },
+  },
+};
 </script>
 
 <style scoped>
+/* scoped: styles only apply to this component */
 
+.user-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  margin-bottom: 8px;
+  background-color: #f5f5f5;
+  border-radius: 4px;
+}
+
+button {
+  background-color: #ff4444;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #cc0000;
+}
 </style>
--- user-manager-app/src/components/UserList.vue
+++ user-manager-app/src/components/UserList.vue
@@ -1,21 +1,46 @@
 <template>
-  <div class="user-list">
-    
+  <div>
+    <p v-if="users.length === 0">No users added yet</p>
+    <ul v-else>
+      <UserItem
+        v-for="(user, index) in users"
+        v-bind:key="index"
+        v-bind:user="user"
+        v-bind:index="index"
+        v-on:delete-user="$emit('delete-user', index)"
+      />
+      <!-- v-on:delete-user listens for event from UserItem and re-emits it UP to App.vue  -->
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'UserList',
-  props: {
-    
+import UserItem from "./UserItem.vue";
+
+export default 
+{
+  name: "UserList",
+  
+  components: 
+  {
+    UserItem,
   },
-  components: {
-    
-  }
-}
+  
+  props: 
+  {
+    users: {
+      type: Array,    
+      required: true, 
+    },
+  },
+};
 </script>
 
 <style scoped>
+/* scoped: styles only apply to this component */
 
+ul {
+  padding: 0;
+  list-style: none;
+}
 </style>
