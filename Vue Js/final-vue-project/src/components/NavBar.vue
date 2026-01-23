04bc22374c8c8b91e610bbd914bbc052ba3805c3
g381n <gebin.official@gmail.com>
2026-01-23 12:48:19 +0530

    feat: add Bootstrap for styling, implement NavBar component, and update main application structure

---
+++ Vue Js/final-vue-project/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-navbar-brand href="/">Hire3X</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Home</b-nav-item>
+                <b-nav-item href="#">About</b-nav-item>
+                <b-nav-item href="#">Contact</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
