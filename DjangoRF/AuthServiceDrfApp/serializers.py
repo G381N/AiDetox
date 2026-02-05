aa9c89c246da3b53a7a0e4b22c98fc8c2b6e651a
g381n <gebin.official@gmail.com>
2026-02-05 12:04:58 +0530

    fix: Remove commented-out import line in serializers.py

--- DjangoRF/AuthServiceDrfApp/serializers.py
+++ DjangoRF/AuthServiceDrfApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+# rest_framewor.mongoengine.serializers
 class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
