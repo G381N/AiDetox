8a4e7a60248db93b57bdf6c02b19b3d81dd18ad5
g381n <gebin.official@gmail.com>
2026-02-08 10:47:10 +0530

    build: Initialize project's Python environment and install core dependencies.

--- Django/Practice/PracticeBackend/crudHandler/views.py
+++ Django/Practice/PracticeBackend/crudHandler/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+  from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import PracticeModel
 from .serializer import PracticeSerializer
