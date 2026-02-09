e9895b193c51ef1305e9e394c32ecfbc71a67869
g381n <gebin.official@gmail.com>
2026-02-09 15:47:51 +0530

    setup all the imports for the class based view that i have made for menu_service

--- Django/IndianFlavours/menu_service/views.py
+++ Django/IndianFlavours/menu_service/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from menu_service.models import Menu
+from menu_service.serializer import MenuSerializer
 
-# Create your views here.
