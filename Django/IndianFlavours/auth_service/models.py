d9c3366de418df538cab8136b712ee6fd3f43e6d
g381n <gebin.official@gmail.com>
2026-02-09 13:42:16 +0530

    added serializer and model for auth service

--- Django/IndianFlavours/auth_service/models.py
+++ Django/IndianFlavours/auth_service/models.py
@@ -1,5 +1,14 @@
-from mongoengine import Document, StringField, DateField
+from mongoengine import Document, StringField, DateField, EmailField
 from DateField import datetime
 
 class User(Document):
-    username=StringField(required=True,unique=True)
+    email         = EmailField(required=True,unique=True)
+    username      = StringField(required=True,unique=True) 
+    password      = StringField(required=True,min_length=10)
+    role          = StringField(choices=["USER,ADMIN"],default="USER")
+    createdAt     = DateTimeField(default=datetime.istnow)
+
+    meta = {
+        "db_alias" : "auth_db", #
+        "collection" : "users"
+    } 
