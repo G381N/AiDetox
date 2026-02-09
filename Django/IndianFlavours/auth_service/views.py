ad42deb058dc7599807e302de30f4d114c43cd57
g381n <gebin.official@gmail.com>
2026-02-09 17:03:16 +0530

    added the login function that was missing

--- Django/IndianFlavours/auth_service/views.py
+++ Django/IndianFlavours/auth_service/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from auth_service.models import User
 from auth_service.serializer import UserSerializer
 from auth_service.security import  hash_password
+from auth_service.security import verify_password
 
 @api_view(["POST"])
 def register(request):
@@ -58,4 +59,42 @@ def register(request):
     status=status.HTTP_201_CREATED
 )
     
-        
+    
+#-----------------------------------------LOGIN----------------------------------------------    
+@api_view(["POST"])
+def login(request):
+    request_username = request.data.get("username")
+    request_password = request.data.get("password")
+
+    if not request_username or not request_password:
+        return Response(
+            {"error": "Username/Email and password are required ..."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    # username OR email login
+    mayby_username_or_email = (
+        User.objects(username=request_username).first()
+        or User.objects(email=request_username.lower()).first()
+    )
+
+    if not mayby_username_or_email:
+        return Response(
+            {"error": "User does not exist ..."},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    if not verify_password(request_password, mayby_username_or_email.password):
+        return Response(
+            {"error": "Invalid password"},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    return Response(
+        {
+            "message": "Login successful ...",
+            "username": user.username,
+            "email": user.email
+        },
+        status=status.HTTP_200_OK
+    )
